177e20d303977e30ef57d7ea4b8d5e83d0c3b7e3
Ian Pratt <ianjdpratt@gmail.com>
2021-09-19 17:02:17 -0500

    yaay added optimization!!!

--- model.py
+++ model.py
@@ -1,5 +1,6 @@
 import lmfit
 import numpy as np
+from scipy.optimize import minimize
 from scipy.integrate import odeint
 import matplotlib.pyplot as plt
 
@@ -85,11 +86,55 @@ class SimpleCovidModel:
 
         return
 
+    def optimizer(self, x, beta_a, beta_b):
+        ret = odeint(
+            self.deriv,
+            self.y0,
+            self.t,
+            args=(
+                beta_a,
+                beta_b,
+                self.params["beta_k"],
+                self.params["gamma"],
+                self.params["rho"],
+            ),
+        )
+
+        totaldeaths = ret.T[3][-1]
+
+        return totaldeaths
+
+    def optimize(self):
+        mod = lmfit.Model(self.optimizer)
+        # params = mod.make_params()
+
+        params = lmfit.Parameters()
+
+        params.add("beta_a", min=0, max=1, value=0.2)
+        params.add("beta_b", min=0, max=1, value=0.2)
+
+        x_data = self.t
+        y_data = self.fit_data
+
+        result = mod.fit(
+            y_data,
+            params,
+            method="least_squares",
+            x=x_data,
+            beta_a=self.params["beta_a"],
+            beta_b=self.params["beta_b"],
+        )
+
+        print(result.fit_report())
+
+        self.params["beta_a"] = params["beta_a"]
+        self.params["beta_b"] = params["beta_b"]
+
     def plot(self):
 
         t = self.t
 
-        fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(11, 5))
+        fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(11, 5))
 
         def set_ax(ax, S, I, R, D):
             # ax.plot(t, S, "g", alpha=0.5, lw=2, label="Susceptible")
@@ -97,6 +142,25 @@ class SimpleCovidModel:
             # ax.plot(t, R, "b", alpha=0.5, lw=2, label="Recovered")
             ax.plot(t, D, "k", alpha=0.5, lw=2, label="Dead")
             ax.plot(t, self.fit_data, "y", alpha=0.5, lw=2, label="True Dead")
+
+            ax.set_xlabel("Time in days")
+            ax.set_ylabel("Number of people per category")
+
+            ax.yaxis.set_tick_params(length=0)
+            ax.xaxis.set_tick_params(length=0)
+
+            legend = ax.legend()
+            legend.get_frame().set_alpha(0.5)
+
+            for spine in ("top", "right", "bottom", "left"):
+                ax.spines[spine].set_visible(False)
+
+        def set_ax_no_truedead(ax, S, I, R, D):
+            # ax.plot(t, S, "g", alpha=0.5, lw=2, label="Susceptible")
+            ax.plot(t, I, "r", alpha=0.5, lw=2, label="Infected")
+            # ax.plot(t, R, "b", alpha=0.5, lw=2, label="Recovered")
+            ax.plot(t, D, "k", alpha=0.5, lw=2, label="Dead")
+            # ax.plot(t, self.fit_data, "y", alpha=0.5, lw=2, label="True Dead")
 
             ax.set_xlabel("Time in days")
             ax.set_ylabel("Number of people per category")
@@ -119,5 +183,11 @@ class SimpleCovidModel:
         S, I, R, D = self.predict()
         set_ax(ax2, S, I, R, D)
         ax2.set_title("after fitting")
+
+        self.optimize()
+
+        S, I, R, D = self.predict()
+        set_ax_no_truedead(ax3, S, I, R, D)
+        ax3.set_title("after optimizing")
 
         plt.show()
